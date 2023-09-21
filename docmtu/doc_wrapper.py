18be0a1f964734a4458d94f7ae1e29a6b7a367a6
Nick Jones <nicolasj@mtu.edu>
2023-09-21 13:51:07 -0400

    VERCEL

--- docmtu/doc_wrapper.py
+++ docmtu/doc_wrapper.py
@@ -5,7 +5,7 @@ import requests
 
 class DOC:
     def __init__(self, site):
-        self.url = "https://api.dineoncampus.com/v1"
+        self.url = "https://doc-relay.vercel.app/v1"
         self.headers = {
             'Accept': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0'
